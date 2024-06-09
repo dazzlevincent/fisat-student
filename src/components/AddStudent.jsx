e8a6e364b3d4a6bdf7982ca5c8462f4c5dc64b72
dazzlevincent <dazzvincent@gmail.com>
2024-06-09 23:03:32 +0530

    Completed

---
+++ src/components/AddStudent.jsx
@@ -0,0 +1,82 @@
+import React, { useState } from 'react'
+import NavBar from './NavBar'
+
+const AddStudent = () => {
+    const [data,setData]=useState(
+        {
+            
+            "firstname":"",
+            "lastname":"",
+            "college":"",
+            "dob":"",
+            "course":"",
+            "mobile":"",
+            "email":"",
+            "address":""
+               
+        }
+    )
+    const inputHandler=(event)=>{
+        setData({...data, [event.target.name]:event.target.value})
+    }
+
+    const readValue=()=>{
+        console.log(data)
+    }
+    return (
+        <div>
+             <NavBar/>
+            <div className="container">
+                <div className="row">
+                    <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+                        <div className="row">
+                            <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+                                <label htmlFor="" className="form-control">First Name:</label>
+                                <input type="text" className="form-control" name='firstname' value={data.firstname} onChange={inputHandler} />
+                            </div>
+                            <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+                                <label htmlFor="" className="form-control">Second Name:</label>
+                                <input type="text" className="form-control" name='lastname' value={data.lastname}  onChange={inputHandler}/>
+                            </div>
+                            <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+                                <label htmlFor="" className="form-control">College:</label>
+                                <input type="text" className="form-control" name='college' value={data.college} onChange={inputHandler} />
+                            </div>
+                            <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+                                <label htmlFor="" className="form-control">Date Of Birth:</label>
+                                <input type="text" className="form-control" name='dob' value={data.dob} onChange={inputHandler} />
+                            </div>
+                            <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+                                <label htmlFor="" className="form-control">Course:</label>
+                                <input type="text" className="form-control" name='course' value={data.course} onChange={inputHandler} />
+                            </div>
+                            <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+                                <label htmlFor="" className="form-control">Mobile No.:</label>
+                                <input type="text" className="form-control" name='mobile' value={data.mobile}  onChange={inputHandler}/>
+                            </div>
+                            <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+                                <label htmlFor="" className="form-control">Email:</label>
+                                <input type="text" className="form-control" name='email' value={data.email} onChange={inputHandler} />
+                            </div>
+                            <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+                                <label htmlFor="" className="form-control">Address:</label>
+                                <input type="text" className="form-control" name='address' value={data.address} onChange={inputHandler}/>
+                            </div>
+                            <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+                                <button className="btn btn-success" onClick={readValue}>SUBMIT</button>
+                                
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+
+
+
+
+        </div>
+    )
+}
+
+export default AddStudent
---
+++ src/components/NavBar.jsx
@@ -0,0 +1,53 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+const NavBar = () => {
+    return (
+        <div>
+
+            <div className="container row">
+                <div className="col col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+                    <div className="row">
+                        <div className="col col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+
+                            <nav className="navbar navbar-expand-lg bg-body-tertiary">
+                                <div className="container-fluid">
+                                    <a className="navbar-brand" href="#">STUDENT FORM</a>
+                                    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                                        <span className="navbar-toggler-icon"></span>
+                                    </button>
+                                    <div className="collapse navbar-collapse" id="navbarNav">
+                                        <ul className="navbar-nav">
+
+                                            <li className="nav-item">
+                                                <Link class="nav-link" to="/">AddStudent</Link>
+                                            </li>
+                                            <li className="nav-item">
+                                                <Link class="nav-link" to="/SearchStudent">SEARCH</Link>
+                                            </li>
+                                            <li className="nav-item">
+                                                <Link class="nav-link" to="/ViewAll">VIEW</Link>
+                                            </li>
+
+
+                                        </ul>
+                                    </div>
+                                </div>
+                            </nav>
+
+
+
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+
+
+
+
+        </div>
+    )
+}
+
+export default NavBar
